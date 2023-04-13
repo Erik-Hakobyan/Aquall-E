fe225c6f50543c21f0b11ff1b31475f52ef57c1c
Erik <eh2642@nyu.edu>
2023-04-13 15:00:19 -0400

    new thruster with GPIO

--- thruster.py
+++ thruster.py
@@ -1,9 +1,22 @@
-import PyESCControl as esc
+import RPi.GPIO as GPIO
 import tkinter as tk
 
-# Initialize the ESCs with the correct channel numbers
-esc1 = esc.PyESCControl(1)
-esc2 = esc.PyESCControl(2)
+# Set the GPIO mode to BCM
+GPIO.setmode(GPIO.BCM)
+
+# Set the GPIO pins for the ESCs
+esc1_pin = 17
+esc2_pin = 18
+
+# Set the frequency for the ESCs
+freq = 50
+
+# Initialize the GPIO pins for the ESCs
+GPIO.setup(esc1_pin, GPIO.OUT)
+GPIO.setup(esc2_pin, GPIO.OUT)
+
+esc1 = GPIO.PWM(esc1_pin, freq)
+esc2 = GPIO.PWM(esc2_pin, freq)
 
 # Create a tkinter window and set its properties
 window = tk.Tk()
@@ -23,29 +36,40 @@ speed1_slider.pack()
 speed2_slider.pack()
 
 # Create six buttons to control the direction of each thruster
-forward1_button = tk.Button(window, text="Thruster 1 Forward", command=lambda: esc1.setSpeed(speed1_slider.get()/100))
-backward1_button = tk.Button(window, text="Thruster 1 Backward", command=lambda: esc1.setSpeed(-speed1_slider.get()/100))
-stop1_button = tk.Button(window, text="Thruster 1 Stop", command=lambda: esc1.setSpeed(0))
-forward2_button = tk.Button(window, text="Thruster 2 Forward", command=lambda: esc2.setSpeed(speed2_slider.get()/100))
-backward2_button = tk.Button(window, text="Thruster 2 Backward", command=lambda: esc2.setSpeed(-speed2_slider.get()/100))
-stop2_button = tk.Button(window, text="Thruster 2 Stop", command=lambda: esc2.setSpeed(0))
+def set_speed1():
+    speed = speed1_slider.get()
+    duty_cycle = speed / 10.0 + 5.0
+    esc1.start(duty_cycle)
+    speed1_label.config(text="Thruster 1: {:.0f}%".format(speed))
+
+def set_speed2():
+    speed = speed2_slider.get()
+    duty_cycle = speed / 10.0 + 5.0
+    esc2.start(duty_cycle)
+    speed2_label.config(text="Thruster 2: {:.0f}%".format(speed))
+
+def stop_esc1():
+    esc1.stop()
+    GPIO.cleanup(esc1_pin)
+    speed1_label.config(text="Thruster 1: 0%")
+
+def stop_esc2():
+    esc2.stop()
+    GPIO.cleanup(esc2_pin)
+    speed2_label.config(text="Thruster 2: 0%")
+
+forward1_button = tk.Button(window, text="Thruster 1 Forward", command=set_speed1)
+backward1_button = tk.Button(window, text="Thruster 1 Backward", command=set_speed1)
+stop1_button = tk.Button(window, text="Thruster 1 Stop", command=stop_esc1)
+forward2_button = tk.Button(window, text="Thruster 2 Forward", command=set_speed2)
+backward2_button = tk.Button(window, text="Thruster 2 Backward", command=set_speed2)
+stop2_button = tk.Button(window, text="Thruster 2 Stop", command=stop_esc2)
 forward1_button.pack()
 backward1_button.pack()
 stop1_button.pack()
 forward2_button.pack()
 backward2_button.pack()
 stop2_button.pack()
-
-# Define a function to update the speed labels with the current speed of each thruster
-def update_speed_labels():
-    speed1 = esc.getSpeed(1) * 100
-    speed2 = esc.getSpeed(2) * 100
-    speed1_label.config(text="Thruster 1: {:.0f}%".format(speed1))
-    speed2_label.config(text="Thruster 2: {:.0f}%".format(speed2))
-    window.after(100, update_speed_labels)
-
-# Call the update_speed_labels() function to start updating the speed labels
-update_speed_labels()
 
 # Start the tkinter event loop
 window.mainloop()
