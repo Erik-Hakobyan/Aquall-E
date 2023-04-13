e46349c5d817dd61ef56bd46c1f7db406510b3da
Erik <eh2642@nyu.edu>
2023-04-13 16:20:08 -0400

    jhb

--- test_thruster.py
+++ test_thruster.py
@@ -2,19 +2,27 @@ import RPi.GPIO as GPIO
 import time
 import tkinter as tk
 
-# Initialize the ESCs with the correct channel numbers
-ESC1_CHANNEL = 13
-ESC2_CHANNEL = 12
+# Initialize the GPIO pins
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(12, GPIO.OUT)
+GPIO.setup(13, GPIO.OUT)
 
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(ESC1_CHANNEL, GPIO.OUT)
-GPIO.setup(ESC2_CHANNEL, GPIO.OUT)
+# Initialize the PWM signals
+esc1_pwm = GPIO.PWM(12, 50)
+esc2_pwm = GPIO.PWM(13, 50)
+esc1_pwm.start(0)
+esc2_pwm.start(0)
 
-esc1 = GPIO.PWM(ESC1_CHANNEL, 50)
-esc2 = GPIO.PWM(ESC2_CHANNEL, 50)
-
-esc1.start(0)
-esc2.start(0)
+# Create a function to set the speed of a thruster
+def set_speed(esc_pwm, speed):
+    if speed > 0:
+        duty_cycle = speed * 4 + 1100
+    elif speed < 0:
+        duty_cycle = speed * 4 + 1900
+    else:
+        duty_cycle = 1500
+    duty_cycle = max(min(duty_cycle, 1900), 1100) # limit duty cycle to 1100-1900
+    esc_pwm.ChangeDutyCycle((duty_cycle-1100)/8) # convert duty cycle to percentage
 
 # Create a tkinter window and set its properties
 window = tk.Tk()
@@ -28,37 +36,35 @@ speed1_label.pack()
 speed2_label.pack()
 
 # Create two sliders to control the speed of each thruster
-speed1_slider = tk.Scale(window, from_=0, to=100, orient=tk.HORIZONTAL, label="Thruster 1 Speed")
-speed2_slider = tk.Scale(window, from_=0, to=100, orient=tk.HORIZONTAL, label="Thruster 2 Speed")
+speed1_slider = tk.Scale(window, from_=-100, to=100, orient=tk.HORIZONTAL, label="Thruster 1 Speed")
+speed2_slider = tk.Scale(window, from_=-100, to=100, orient=tk.HORIZONTAL, label="Thruster 2 Speed")
 speed1_slider.pack()
 speed2_slider.pack()
 
 # Create six buttons to control the direction of each thruster
-def set_speed1(speed):
-    duty_cycle = speed * 2 + 50
-    esc1.ChangeDutyCycle(duty_cycle)
-    speed1_label.config(text="Thruster 1: {:.0f}%".format(speed))
-
-def set_speed2(speed):
-    duty_cycle = speed * 2 + 50
-    esc2.ChangeDutyCycle(duty_cycle)
-    speed2_label.config(text="Thruster 2: {:.0f}%".format(speed))
-
-speed1_slider.bind("<B1-Motion>", lambda event: set_speed1(speed1_slider.get()))
-speed2_slider.bind("<B1-Motion>", lambda event: set_speed2(speed2_slider.get()))
-
-forward1_button = tk.Button(window, text="Thruster 1 Forward", command=lambda: set_speed1(100))
-backward1_button = tk.Button(window, text="Thruster 1 Backward", command=lambda: set_speed1(0))
-stop1_button = tk.Button(window, text="Thruster 1 Stop", command=lambda: set_speed1(50))
-forward2_button = tk.Button(window, text="Thruster 2 Forward", command=lambda: set_speed2(100))
-backward2_button = tk.Button(window, text="Thruster 2 Backward", command=lambda: set_speed2(0))
-stop2_button = tk.Button(window, text="Thruster 2 Stop", command=lambda: set_speed2(50))
+forward1_button = tk.Button(window, text="Thruster 1 Forward", command=lambda: set_speed(esc1_pwm, speed1_slider.get()))
+backward1_button = tk.Button(window, text="Thruster 1 Backward", command=lambda: set_speed(esc1_pwm, -speed1_slider.get()))
+stop1_button = tk.Button(window, text="Thruster 1 Stop", command=lambda: set_speed(esc1_pwm, 0))
+forward2_button = tk.Button(window, text="Thruster 2 Forward", command=lambda: set_speed(esc2_pwm, speed2_slider.get()))
+backward2_button = tk.Button(window, text="Thruster 2 Backward", command=lambda: set_speed(esc2_pwm, -speed2_slider.get()))
+stop2_button = tk.Button(window, text="Thruster 2 Stop", command=lambda: set_speed(esc2_pwm, 0))
 forward1_button.pack()
 backward1_button.pack()
 stop1_button.pack()
 forward2_button.pack()
 backward2_button.pack()
 stop2_button.pack()
+
+# Define a function to update the speed labels with the current speed of each thruster
+def update_speed_labels():
+    speed1 = speed1_slider.get()
+    speed2 = speed2_slider.get()
+    speed1_label.config(text="Thruster 1: {:.0f}%".format(speed1))
+    speed2_label.config(text="Thruster 2: {:.0f}%".format(speed2))
+    window.after(100, update_speed_labels)
+
+# Call the update_speed_labels() function to start updating the speed labels
+update_speed_labels()
 
 # Start the tkinter event loop
 window.mainloop()
